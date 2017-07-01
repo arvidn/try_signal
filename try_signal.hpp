803e8cef349672696031c00ac2a28759c3b1458b
arvidn <arvid@cs.umu.se>
2017-07-01 10:08:55 -0400

    fix redefinition warning

--- try_signal.hpp
+++ try_signal.hpp
@@ -40,7 +40,9 @@ POSSIBILITY OF SUCH DAMAGE.
 #include "signal_error_code.hpp"
 
 #ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
 
 #ifdef __GNUC__
